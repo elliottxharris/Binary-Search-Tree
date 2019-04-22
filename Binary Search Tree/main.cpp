d60b6815c0c74ed783f22ff7b081cda147ece0ae
elliottxharris <elliottxharris@gmail.com>
2019-04-22 12:42:56 -0500

    Initial Commit

---
+++ Binary Search Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Binary Search Tree
+//
+//  Created by Elliott Harris on 4/22/19.
+//  Copyright © 2019 Elliott Harris. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
